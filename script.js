faebad48a54fa5929027e6a12e8c630350e5ade1
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-09-30 17:23:15 -0400

    minor changes

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/*
+var i=0;
+var txt="Vel commodi perspiciatis hic voluptatem ducimus sit magnam illum non blanditiis temporibus est ratione voluptas 33 doloribus nulla!"
+var speed=10;//in milliseconds
+
+function typeWriter() {
+    if (i<txt.length){
+        document.getElementById("typeout").innerHTML+=txt.charAt(i);
+        i++;
+        setTimeout(typeWriter,speed);
+    }
+}
+typeWriter()*/
