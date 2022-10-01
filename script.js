1d525db17b1fa85109022f45214616009e6f9b6a
imapotatoes11 <79340132+imapotatoes11@users.noreply.github.com>
2022-10-01 12:03:08 -0400

    minor changes

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function typeWriter() {
     }
 }
 typeWriter()*/
+
+
+function typeWrite(id, message, speed) {
+    console.log("fsdfa")
+    var i = 0;
+    var txt=message
+    var speed=speed;
+    var elem = document.getElementById(id);
+    function typeWriter() {
+        if (i<txt.length){
+            elem.innerHTML+=txt.charAt(i);
+            i++;
+            setTimeout(typeWriter,speed);
+        }
+    }
+}
+
+
+
+//Scroll to top
+function scrollTop(){
+    window.scrollTo(0,0)
+}
